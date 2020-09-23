48f267b0a420afb5751755d2923cc38a4abfce67
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2020-09-22 20:12:42 -0400

    Create a LIFO struct

---
+++ stack/stack.go
@@ -0,0 +1,67 @@
+package stack
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	Value float64
+	next *Node
+}
+
+func (n *Node) String() string {
+	return fmt.Sprint(n.Value)
+}
+
+type Stack struct {
+	head *Node // first off
+	count int
+}
+
+func (s *Stack) Push(v float64) {
+	n := Node{v, s.head}
+	s.count++
+	s.head = &n
+}
+
+func (s *Stack) Pop() *Node {
+	if s.count == 0 {
+		return nil
+	}
+	s.count--
+
+	head := s.head
+	s.head = head.next
+	return head
+}
+
+func (s *Stack) Shuffle() {
+	if s.count < 1 {
+		return
+	}
+
+	oldHead := s.head
+	newHead := oldHead.next
+
+	oldHead.next = newHead.next
+	newHead.next = oldHead
+	s.head = newHead
+}
+
+func (s *Stack) Pick(n int) *Node {
+	if n > s.count {
+		return nil
+	}
+
+	oldHead := s.head
+	temp := s.head.next
+	for i := 0; i < n - 1; i++ {
+		temp = temp.next
+	}
+
+
+	newHead := temp
+	newHead.next = oldHead
+	s.head = newHead
+	return s.Pop()
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,42 @@
+package stack
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := Stack{}
+	s.Push(1.0)
+	s.Push(2.0)
+	want := 3.0
+	s.Push(want)
+
+	if got := s.Pop(); got.Value != want {
+		t.Errorf("Stack Pop = %v, want %v", got, want)
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	s := Stack{}
+	s.Push(1.0)
+	want := 2.0
+	s.Push(want)
+	s.Push(3.0)
+	s.Shuffle()
+
+	if got := s.Pop(); got.Value != want {
+		t.Errorf("Stack Pop = %v, want %v", got, want)
+	}
+}
+
+func TestPick(t *testing.T) {
+	s := Stack{}
+	want := 1.0
+	s.Push(want)
+	s.Push(2.0)
+	s.Push(3.0)
+
+	if got := s.Pick(2); got.Value != want {
+		t.Errorf("Stack Pop = %v, want %v", got, want)
+	}
+}
